fd00a3f87bb9875c03a66d3b73877d534c10b510
xEAGLEx98 <poncec296@gmail.com>
2023-11-03 11:00:28 -0600

    Corrección de versiones

---
+++ PresenteacionCarCare/ACarCare/AccesoVehiculos.cs
@@ -0,0 +1,29 @@
+﻿using ConectarBd;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACarCare
+{
+    internal class AccesoVehiculos : ICrud
+    {
+        Base baseDatos = new Base("localhost", "root", "", "car_care");
+        public void Borrar(dynamic entidad)
+        {
+            baseDatos.Comando(String.Format("CALL eliminar_vehiculos({0})", entidad.IdVehiculo));
+        }
+
+        public void Guardar(dynamic entidad)
+        {
+            baseDatos.Comando(String.Format("CALL insertar_vehiculos('{0}', '{1}', '{2}', {3})", entidad.Marca, entidad.Modelo, entidad.TipoVehiculo, entidad.IdVehiculo));
+        }
+
+        public DataSet Mostrar(string filtro)
+        {
+            return baseDatos.Obtener(string.Format("call mostrar_vehiculos('%{0}%')", filtro), "vehiculos");
+        }
+    }
+}
---
+++ PresenteacionCarCare/ACarCare/ICrud.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACarCare
+{
+    public interface ICrud
+    {
+        void Guardar(dynamic entidad);
+        void Borrar(dynamic entidad);
+        DataSet Mostrar(string filtro);
+    }
+}
